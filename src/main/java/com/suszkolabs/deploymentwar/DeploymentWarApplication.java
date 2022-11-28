f7ed28f50190ec7eb63f118102ea2e8db53afbf3
Woojciech <wojtek@suszko.pl>
2022-11-28 11:55:02 +0100

    Initial commit

---
+++ src/main/java/com/suszkolabs/deploymentwar/DeploymentWarApplication.java
@@ -0,0 +1,13 @@
+package com.suszkolabs.deploymentwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeploymentWarApplication.class, args);
+    }
+
+}
